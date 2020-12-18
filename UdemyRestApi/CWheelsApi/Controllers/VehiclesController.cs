36f9d620beb3cd5e981f0b8b9f06db83eaf1f50e
Jonathan Vlk <jvlk@wmp.com>
2020-12-18 15:17:29 -0600

    added vehicles GET and POST

---
+++ UdemyRestApi/CWheelsApi/Controllers/VehiclesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CWheelsApi.Models;
+
+namespace CWheelsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        private static List<Vehicle> vehicles = new List<Vehicle>
+        {
+            new Vehicle(){ Id = 0, Title = "Tesla S", Price = 23000 },
+            new Vehicle(){ Id = 1, Title = "Tesla X", Price = 29000 }
+        };
+
+        [HttpGet]
+        public IEnumerable<Vehicle> Get()
+        {
+            return vehicles;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Vehicle vehicle)
+        {
+            vehicles.Add(vehicle);
+        }
+    }
+}
